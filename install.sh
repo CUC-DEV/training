5ff9d3d860ba0a0eba4d9f43e54534e06f81cf71
Mengyu <mengyu.dev@outlook.com>
2015-10-12 23:47:38 +0200

    reload zshrc

--- install.sh
+++ install.sh
@@ -11,6 +11,7 @@ wget -qO- https://raw.githubusercontent.com/creationix/nvm/v0.29.0/install.sh | 
 echo It is going to install nodejs
 nvm install 4.1.2
 echo It is going to install om-my-zsh
+source ~/.zshrc
 sudo sh -c "$(wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
 
 echo The install was successful. 
