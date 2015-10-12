4d6a5af7b1f99a034fb4c387560d05318f392c7f
Mengyu <mengyu.dev@outlook.com>
2015-10-12 23:28:55 +0200

    add install.sh

---
+++ install.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+if [ $USER != 'root' ]
+then
+echo "REQUIRES ROOT"
+exit 0
+fi
+echo It is going to install zsh and git-core
+sudo apt-get update && sudo apt-get install zsh git-core
+echo It is going to install om-my-zsh
+sudo sh -c "$(wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+echo It is going to install nvm
+wget -qO- https://raw.githubusercontent.com/creationix/nvm/v0.29.0/install.sh | bash
+echo It is going to install nodejs
+nvm install 4.1.2
+
+echo The install was successful. 
