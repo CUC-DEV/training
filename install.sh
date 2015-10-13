c056268d1607db132ac37ee7ae89b39df8db71dc
mengyu-dev <mengyu.dev@outlook.com>
2015-10-13 20:07:33 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,8 +6,8 @@ exit 0
 fi
 echo It is going to install git-core
 sudo apt-get update && sudo apt-get install zsh git-core
-echo It is going to install nvm
-wget -qO- https://raw.githubusercontent.com/creationix/nvm/v0.29.0/install.sh | bash
 echo It is going to install om-my-zsh
 sh -c "$(wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+echo It is going to install nvm
+wget -qO- https://raw.githubusercontent.com/creationix/nvm/v0.29.0/install.sh | bash
 echo The install was successful. 
