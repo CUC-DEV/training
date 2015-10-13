345900e6648298a59b3feca56c673d75caa7d2da
mengyu-dev <mengyu.dev@outlook.com>
2015-10-13 13:36:12 +0200

    Update install.sh
    remove sudo for zsh

--- install.sh
+++ install.sh
@@ -9,5 +9,5 @@ sudo apt-get update && sudo apt-get install zsh git-core
 echo It is going to install nvm
 wget -qO- https://raw.githubusercontent.com/creationix/nvm/v0.29.0/install.sh | bash
 echo It is going to install om-my-zsh
-sudo sh -c "$(wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+sh -c "$(wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
 echo The install was successful. 
