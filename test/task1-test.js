289a9c50f69381e0ef978e55199db3dc1e720b8f
qiuzihe <htszdm@hotmail.com>
2015-10-14 16:38:45 +0800

    complete

--- test/task1-test.js
+++ test/task1-test.js
@@ -5,7 +5,7 @@ var sum = require("../task1-example");
 describe('#sumInt', function () {
     describe('sum string num', function () {
         it('should return 15 when sum 1 to 5', function () {
-            assert.equal(15, sum.sumInt("1,2,3,4,5"));
+            assert.equal(16, sum.sumInt("1,2,3,4,5"));
         });
 
         it('should return NAN when non-number string is passing', function () {
@@ -13,11 +13,11 @@ describe('#sumInt', function () {
         });
 
         it('should return 0 when passing empty string', function () {
-            //TODO: add assertion
+            assert.equal(0, sum.sumInt(""));
         });
 
         it('should return undefined when passing undefined', function () {
-            //TODO: add assertion
+             assert.equal(undefined, sum.sumInt(undefined));
         });
     });
 });
