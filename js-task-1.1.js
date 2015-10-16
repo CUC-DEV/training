1e74a1480d4bb2494b780bc65274addeb4035a06
yingzefeng <506439804@qq.com>
2015-10-16 23:21:30 +0800

    task1

--- js-task-1.1.js
+++ js-task-1.1.js
@@ -1,17 +1,29 @@
 // JavaScript Document
-var a="123,23,21,23,12";
+
+function sumInt(str)
+{
+	var a=str//"123,23,21,23,12";
 var sum=0;
 var i=0;
 	var n=0;
 	var k=[0,0,0,0];
-function sumInt(str)
-{
-	
+	if(str==" ")
+	{
+		return sum=0;
+	}
+	if(str==undefined)
+	{
+		return undefined;
+	}
 	for(i=0;i<a.length;i++)
 	{
 		if(a[i]===',')
 		{	k[n]=i;
 			n=n+1;
+		}
+		if((a[i]>'9')||(a[i]<'0')&&(a[i]!=','))
+		{
+			return NaN;
 		}
 	}
 	var b=a.substring(0,k[0])
@@ -23,7 +35,10 @@ function sumInt(str)
 	}
 	var b=a.substring(k[3]+1);
 		sum=sum+parseInt(b);
+		return sum;
 }
-alert('数字字符串为“123,23,21,23,12”');
-sumInt(a);
-sum
+//alert('数字字符串为“123,23,21,23,12”');
+//sumInt(a);
+//sum
+
+exports.sumInt = sumInt;
--- test/task1-test.js
+++ test/task1-test.js
@@ -1,25 +1,25 @@
 'use strict'
 
 var assert = require("assert");
-var sum = require("../task1-example");
+var ap= require("../js-task-1.1");
 describe('#sumInt', function () {
     describe('sum string num', function () {
-        it('should return 15 when sum 1 to 5', function () {
-            assert.equal(15, sum.sumInt("1,2,3,4,5"));
+        it('should return 202 when sum 1 to 5', function () {
+            assert.equal(15, ap.sumInt("1,2,3,4,5"));
         });
-
+        
         it('should return NAN when non-number string is passing', function () {
-            assert.equal(true, isNaN(sum.sumInt("1,2,3,4,5s")));
+            assert.equal(true, isNaN(ap.sumInt("1,2,3,4,5s")));
         });
 
         it('should return 0 when passing empty string', function () {
             //TODO: add assertion
-            assert.equal(0,sum.sumInt(" "));
+            assert.equal(0,ap.sumInt(" "));
         });
 
         it('should return undefined when passing undefined', function () {
             //TODO: add assertion
-           assert.equal(undefined,sum.sumInt(undefined))
+           assert.equal(undefined,ap.sumInt(undefined))
         });
     });
 });
