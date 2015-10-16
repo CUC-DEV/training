8b4348c73472c2050e4b27b3cbee2e0d2ea1ab09
Fusizhe <Fusizhe@hotmail.com>
2015-10-17 05:15:14 +0800

    just change the code of task 3 without test

--- js-task-1.3.js
+++ js-task-1.3.js
@@ -1,113 +1,90 @@
-var Student={
-	name:"null",
-	course:Course,
-	j:0,
-	enroll: function(course)
+function Student(stuname,coursename)
+{
+	this.stuname=stuname;
+	this.coursename=new Array();
+	this.course=null;
+	this.enroll=function(course)
+	{
+		this.coursename[this.coursename.length]=course.coursename;
+		this.course=course.coursename;
+		course.add(this);
+		this.course=course;
+	}
+	this.abandon=function(course)
+	{
+		var i=0;
+		for(cou in this.coursename)
+		{
+			if(cou==course.coursename)
+			{
+				course.del(this);
+				for(;i<this.coursename.length-2;i++)
+				{
+					this.coursename[i]=this.coursename[i+1];
+				}
+				break;
+			}
+			i++;
+		}
+	}
+	this.getTeachers=function()
+	{
+		var obj={};
+		for(cou in this.coursename)
+		{
+			obj[cou]=this.course.teaname;
+		}
+		return obj;
+	}
+	return this;
+}
+
+function Teacher(teaname,coursename)
+{
+	this.teaname=teaname;
+	this.coursename=coursename;
+	this.course=null;
+	this.instruct=function(course)
 	{
 		this.course=course;
-		course.student[course.i]=this;
-		this.j=course.i;
-		course.i++;
-	},
-	
-	abandon: function(course)
+		this.coursename=course.coursename;
+		course.teaname=this.teaname;		
+	}
+	this.getstudents=function()
 	{
-		this.course=Course;
-		for(var k=j;k<i-1;k++)
-		{
-			course.student[k]=course.student[k+1];
-		}
-		course.i--;
-		j=0;
-	},
-	
-	getTeachers: function()
-	{
-		var obj={
-			course:this.course,
-			teacher:[this.course.teacher1.name,this.course.teacher2.name,this.course.teacher3.name]
-		}
+		var obj={}
+		obj[this.coursename]=this.course.student;
 	}
 }
 
-var Teacher={
-	name:"null",
-	course:Course,
-	instruct: function(course)
+function Course(coursename,stuname,teaname)
+{
+	this.coursename=coursename;
+	this.student=new Array();
+	this.teaname=teaname;
+	this.add=function(student)
 	{
-		course.set(this);
-	},
+		this.student[this.student.length;]=student.stuname;
+	}
+	this.del=function(student)
+	{
+		var i=0;
+		for(stu in this.student)
+		{
+			if(stu==stu.stuname)
+			{
+				for(;i<this.coursename.length-2;i++)
+				{
+					this.coursename[i]=this.coursename[i+1];
+				}
+				break;
+			}
+			i++;
+		}
+	}
+	this.set=function(teacher)
+	{
+		teacher.instruct(this);
+	}
+}
 	
-	getStudents: function()
-	{
-		var obj={
-			course:this.course,
-			sutdent:[this.course.student[0].name,this.course.student[1].name,this.course.student[2].name]
-		}
-		
-	}
-}
-
-
-var Course={
-	name:"null",
-	teacher:[Teacher,Teacher,Teacher],
-	student:[Student,Student,Student],
-	i:0,
-	set: function(teacher)
-	{
-		teacher.course=this;
-		if(this.teacher[0]==Teacher)
-		{
-			this.teacher[0]=teacher;
-		}
-		else if(this.teacher[1]==Teacher)
-		{
-			this.teacher[1]=teacher
-		}
-		else if(this.teacher[2]==Teacher)
-		{
-			this.teacher[2]=teacher
-		}
-	}
-}
-
-var student1={
-	name:"student1"
-}
-var student2={
-	name:"student2"
-}
-var student3={
-	name:"student3"
-}
-student1.__proto__=Student;
-student2.__proto__=Student;
-student3.__proto__=Student;
-
-var teacher1={
-	name:"teacher1"
-}
-var teacher2={
-	name:"teacher2"
-}
-var teacher3={
-	name:"teacher3"
-}
-teacher1.__proto__=Teacher;
-teacher2.__proto__=Teacher;
-teacher3.__proto__=Teacher;
-
-var course1={
-	name:"course1"
-}
-var course2={
-	name:"course2"
-}
-var course3={
-	name:"course3"
-}
-course1.__proto__=Course;
-course2.__proto__=Course;
-course3.__proto__=Course;
-
