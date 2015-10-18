fa2c3fcfc5c32c4a4c50f84acef829ebbaf0e02e
qiuzihe <htszdm@hotmail.com>
2015-10-18 19:52:32 +0800

    add test

--- test/test.js
+++ test/test.js
@@ -32,6 +32,57 @@ describe('#math', function () {
             assert.equal(1, c.r);
             assert.equal(-2, c.i);
             assert.equal('1-2i', c.toString());
-        });
+        });  
     });
+    describe('method of add in complex', function () {
+         it('should return complex(2) when parse 1,+,1', function () {
+            var c1 = new math.Complex.of('1');
+            var c2= new math.Complex.of('1');
+            var result= c1.add(c2);
+            assert.equal(2, result.r);
+            assert.equal(0, result.i);
+            assert.equal('2+0i', result.toString());
+         });  
+          it('should return complex(2) when parse 1,+,1+2i', function () {
+            var c1 = new math.Complex.of('1');
+            var c2= new math.Complex.of('1+2i');
+            var result= c1.add(c2);
+            assert.equal(2, result.r);
+            assert.equal(2, result.i);
+            assert.equal('2+2i', result.toString());
+         });  
+        
+    
+    
+    });
+    describe('method of substract in complex', function () {
+         it('should return complex(2) when parse 1,-,1', function () {
+            var c1 = new math.Complex.of('1');
+            var c2= new math.Complex.of('1');
+            var result= c1.substract(c2);
+            assert.equal(0, result.r);
+            assert.equal(0, result.i);
+            assert.equal('0+0i', result.toString());
+         });  
+          it('should return complex(2) when parse 2+2i,-,1', function () {
+            var c1 = new math.Complex.of('2+2i');
+            var c2= new math.Complex.of('1');
+            var result= c1.substract(c2);
+            assert.equal(1, result.r);
+            assert.equal(2, result.i);
+            assert.equal('1+2i', result.toString());
+         });  
+        
+    
+    
+    });
+    
+    
+    
+    
+    
+    
+    
+    
+    
 });
